8e766e780b764625c84c55f797d2f890e4f8c3f1
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2025-02-25 21:32:26 +0000

    renamed

---
+++ dag_sets/default/1000/dag_0792.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+from airflow.models import DAG
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dag_0792",
+    start_date=datetime(2021, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    task_000 = EmptyOperator(task_id="task_000")
+    task_001 = EmptyOperator(task_id="task_001")
+    task_002 = EmptyOperator(task_id="task_002")
+    task_003 = EmptyOperator(task_id="task_003")
+    task_004 = EmptyOperator(task_id="task_004")
+
+    task_000 >> task_001    task_001 >> task_002    task_002 >> task_003    task_003 >> task_004
